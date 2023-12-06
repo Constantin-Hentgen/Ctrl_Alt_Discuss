1687ed16eda082e26f1892c83572d376173ac661
Constantin Hentgen <constantin.hentgen@proton.me>
2023-12-06 15:32:33 +0100

    global upgrade of the content generation

--- src/content_generator.py
+++ src/content_generator.py
@@ -1,11 +1,24 @@
 from openai import OpenAI
 from json import loads
+from time import sleep
 
 # local imports
-from secrets import OPENAI_API_KEY_THB as OPENAI_API_KEY
+from secrets import OPENAI_API_KEY_PERSO as OPENAI_API_KEY
+from configuration import (
+    PREPROMPT_METADATA,
+    PROMPT_CONCLUSION,
+    PROMPT_INTRODUCTION,
+    PROMPT_DEVELOPMENT,
+    PROMPT_PLAN,
+    PREPROMPT,
+    GPT_MODEL,
+)
 
 
-def generate_content(model: str, preprompt: str, prompt: str) -> str:
+# generalist openai request
+def generate_content(
+    prompt: str, preprompt: str = PREPROMPT, model: str = GPT_MODEL
+) -> str:
     client = OpenAI(api_key=OPENAI_API_KEY)
     response = client.chat.completions.create(
         model=model,
@@ -20,3 +33,46 @@ def generate_content(model: str, preprompt: str, prompt: str) -> str:
     )
 
     return loads(response.choices[0].message.content)
+
+
+def generate_plan() -> list:
+    return generate_content(prompt=PROMPT_PLAN)
+
+
+def generate_metadata(plan: dict) -> dict:
+    return generate_content(preprompt=PREPROMPT_METADATA, prompt=str(plan))
+
+
+def generate_development(plan: dict) -> dict:
+    plan_list = plan["plan"]
+    development = []
+    for part in plan_list:
+        development.append(generate_content(prompt=f"{PROMPT_DEVELOPMENT}\npart"))
+
+    return development
+
+
+def generate_introduction(plan: dict) -> dict:
+    return generate_content(prompt=PROMPT_INTRODUCTION)
+
+
+def generate_conclusion() -> dict:
+    return generate_content(prompt=PROMPT_CONCLUSION)
+
+
+def generate_podcast() -> dict:
+    plan = generate_plan()
+    metadata = generate_metadata(plan)
+    introduction = generate_introduction(plan)
+    development = generate_development(plan)
+    conclusion = generate_conclusion()
+
+    return {
+        "title": metadata["title"],
+        "description": metadata["description"],
+        "thumbnail_prompt": f"{metadata['thumbnail_prompt']}\n NO WRITING or INSCRIPTIONS",
+        "folder_name": metadata["folder_name"],
+        "script": introduction["script"]
+        + [line for chapter in development for line in chapter["script"]]
+        + conclusion["script"],
+    }
--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ from time import sleep
 from json import dump
 
 # local imports
-from configuration import GPT_MODEL, PRE_PROMPT, prompt, OUTPUT_PATH
 from thumbnail_generator import generate_thumbnail
-from content_generator import generate_content
+from content_generator import generate_podcast
+from configuration import OUTPUT_PATH
 from soundfile_management import (
     generate_audio_openai,
     generate_audio_xi_labs,
@@ -21,35 +21,33 @@ if __name__ == "__main__":
     # let the time to initialize the prompt variable and get the article content
     sleep(5)
 
-    podcast_content = generate_content(
-        model=GPT_MODEL, preprompt=PRE_PROMPT, prompt=prompt
-    )
+    podcast_content = generate_podcast()
 
-    folder_name = unidecode(podcast_content["folder_name"].replace(" ", "_"))
+    folder_name = unidecode(podcast_content["folder_name"])
     folder_path = f"{OUTPUT_PATH}\\{folder_name}"
     songs_folder = f"{folder_path}\\voices"
     makedirs(f"{folder_path}\\voices")
 
     generate_thumbnail(
-        prompt=f"{podcast_content['thumbnail_prompt']} (avoid writings and characters in the generation)",
+        prompt=podcast_content["thumbnail_prompt"],
         path=folder_path,
     )
 
     for index, item in enumerate(podcast_content["script"]):
-        # generate_audio_openai(
-        #     script=item["line"],
-        #     voice="onyx" if item["name"] == "Alex" else "nova",
-        #     index=index + 1,
-        #     folder_path=folder_path,
-        #     sound_format="mp3",
-        # )
-        generate_audio_xi_labs(
+        generate_audio_openai(
             script=item["line"],
-            voice="Michael" if item["name"] == "Michael" else "Bella",
+            voice="echo" if item["name"] == "Michael" else "nova",
             index=index + 1,
             folder_path=folder_path,
             sound_format="mp3",
         )
+        # generate_audio_xi_labs(
+        #     script=item["line"],
+        #     voice="Michael" if item["name"] == "Michael" else "Matilda",
+        #     index=index + 1,
+        #     folder_path=folder_path,
+        #     sound_format="mp3",
+        # )
         sleep(30)
 
     merge_sound_file(folder_path=folder_path)
